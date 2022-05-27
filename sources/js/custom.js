5bcac197894c65702161493a4444c6d73b060a89
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-05-27 20:35:14 +0100

    I am done with the site and I have made it responsive.

--- sources/js/custom.js
+++ sources/js/custom.js
@@ -119,15 +119,31 @@ $(".featured-slider").slick({
     ]
 })
 
-$(".steps-slider").slick({
+// $(".steps-slider").slick({
+//     // variableWidth: true,
+//     infinite: false,
+//     // autoplay: true,
+//     centerMode: true,
+//     arrows: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     prevArrow: "<button class='flex bg-[#D8D8D8] absolute top-1/2 translate-x-1/2 -translate-y-1/2 -left-14 w-8 h-12 px-1 items-center justify-center py-[6px] next-arrow text-white hover:bg-brand-blue transition-all duration-100 ease-linear hover:duration-150 group'><img src='images/left-caret.svg'></button>",
+//     nextArrow: "<button class='flex bg-[#D8D8D8] absolute top-1/2 translate-x-1/2 -translate-y-1/2 -right-10 w-8 h-12 px-1 items-center justify-center py-[6px] next-arrow text-white hover:bg-brand-blue transition-all duration-100 ease-linear hover:duration-150 group'><img src='images/right-caret.svg'></button>"
+// })
+
+$(".steps-slider").owlCarousel({
     // variableWidth: true,
     infinite: false,
     // autoplay: true,
-    arrows: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: "<button class='flex bg-[#D8D8D8] absolute top-1/2 translate-x-1/2 -translate-y-1/2 -left-14 w-8 h-12 px-1 items-center justify-center py-[6px] next-arrow text-white hover:bg-brand-blue transition-all duration-100 ease-linear hover:duration-150 group'><img src='images/left-caret.svg'></button>",
-    nextArrow: "<button class='flex bg-[#D8D8D8] absolute top-1/2 translate-x-1/2 -translate-y-1/2 -right-10 w-8 h-12 px-1 items-center justify-center py-[6px] next-arrow text-white hover:bg-brand-blue transition-all duration-100 ease-linear hover:duration-150 group'><img src='images/right-caret.svg'></button>"
+    center: true,
+    nav: true,
+    items: 1,
+    slideBy: 1,
+    loop: true,
+    navText: [
+        "<button class='flex bg-[#D8D8D8] absolute top-1/2 translate-x-1/2 -translate-y-1/2 -left-4 lg:left-0 w-8 h-12 px-1 items-center justify-center py-[6px] next-arrow text-white hover:bg-brand-blue transition-all duration-100 ease-linear hover:duration-150 group'><img src='images/left-caret.svg'></button>",
+        "<button class='flex bg-[#D8D8D8] absolute top-1/2 translate-x-1/2 -translate-y-1/2 right-3 w-8 h-12 px-1 items-center justify-center py-[6px] next-arrow text-white hover:bg-brand-blue transition-all duration-100 ease-linear hover:duration-150 group'><img src='images/right-caret.svg'></button>"
+    ]
 })
 
 $(document).ready(function(){
